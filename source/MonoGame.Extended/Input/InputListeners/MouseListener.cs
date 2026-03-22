ddaa052f2324d1c10188435d96fe6e6ed643375e
Lexi Rose <north@tinymagnet.com>
2026-03-21 20:24:30 -0500

    Make DragThreshold property settable
    Change DragThreshold from a read-only property to a read-write property (added setter) to allow modifying the drag threshold at runtime.

--- source/MonoGame.Extended/Input/InputListeners/MouseListener.cs
+++ source/MonoGame.Extended/Input/InputListeners/MouseListener.cs
@@ -44,7 +44,7 @@ namespace MonoGame.Extended.Input.InputListeners
         public ViewportAdapter ViewportAdapter { get; }
 
         public int DoubleClickMilliseconds { get; }
-        public int DragThreshold { get; }
+        public int DragThreshold { get; set; }
 
         /// <summary>
         ///     Returns true if the mouse has moved between the current and previous frames.
